1a3377e34d255e51ee4fc5b43a5caaa352659b69
Jon Graf <jgraf@vermeer.com>
2026-09-08 20:29:28 -0500

    removed cruft from ms template

--- SystemYouWishYouHad/System.API/Program.cs
+++ SystemYouWishYouHad/System.API/Program.cs
@@ -1,48 +1,22 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using SystemAPI.Config;
+using System.API.Config;
+using System.Shared.Config;
+using System.Data.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var settings = builder.Configuration.Get<AppSettings>();
 
 builder.Services.AddOpenApi();
+builder.Services.ConfigUserDb(settings);
 builder.Services.AddCorsConfig(settings);
 
 var app = builder.Build();
 
-app.UseCors("CorsPolicy");
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
+if (app.Environment.IsDevelopment())
+    app.MapOpenApi();
+
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-//     {
-//         var forecast = Enumerable.Range(1, 5).Select(index =>
-//                 new WeatherForecast
-//                 (
-//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                     Random.Shared.Next(-20, 55),
-//                     summaries[Random.Shared.Next(summaries.Length)]
-//                 ))
-//             .ToArray();
-//         return forecast;
-//     })
-//     .WithName("GetWeatherForecast");
-
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
