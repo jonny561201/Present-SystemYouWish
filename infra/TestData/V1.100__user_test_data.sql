43c01bfc04c7072463121194ee27afd897221874
Jon Graf <jgraf@vermeer.com>
2026-09-09 15:13:25 -0500

    added test data for local demo

---
+++ infra/TestData/V1.100__user_test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (given_name, middle_name, family_name, email) VALUES 
+                    ('Jonathon', 'M', 'Graf', 'jongraf@gmail.com'),
+                    ('Leslie', 'L', 'Neilson', 'leslineilson@aol.com'),
+                    ('Ash', 'J', 'Williams', 'hailToTheKing@gmail.com'),
+                    ('Peter', NULL, 'Venkman', 'CameSawKickedItsAzz@gmail.com'),
+                    ('Judge', NULL, 'Dredd', 'liveInDredd@gmail.com'),
+                    ('Fred', 'Drop', 'Dead', 'dropDeadFred@netscape.com');
