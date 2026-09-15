241985715a62351b2f1e7629e00125cf6aa9bf2f
Jon Graf <jgraf@vermeer.com>
2026-09-15 12:03:15 -0500

    simplified the endpoints to keep it moving

--- SystemYouWishYouHad/WishSystem.API/Endpoints/ApiEndpoints.cs
+++ SystemYouWishYouHad/WishSystem.API/Endpoints/ApiEndpoints.cs
@@ -6,22 +6,11 @@ public static class ApiEndpoints
 {
     public static WebApplication RegisterEndpoints(this WebApplication app)
     {
-        app.MapGet("/health", () => "healthy");
+        app.MapGet("/users", (IUserService service) => service.GetUsers());
         
-        app.MapGet("/users", (IUserService service) =>
-        {
-            return service.GetUsers();
-        });
+        app.MapGet("/users/{id:guid}", (IUserService service, Guid id) => service.GetUser(id));
         
-        app.MapGet("/users/{id:guid}", (IUserService service, Guid id) =>
-        {
-            return service.GetUser(id);
-        });
-        
-        app.MapPost("/users/submit", (IUserService service, List<Guid> userId) =>
-        {
-            return service.SubmitUsers(userId);
-        });
+        app.MapPost("/users/submit", (IUserService service, List<Guid> userId) => service.SubmitUsers(userId));
         
         return app;
     }
