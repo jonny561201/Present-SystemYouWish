220ef28d8a949e283632e8cc4b965060fed6dc65
Jon Graf <jgraf@vermeer.com>
2026-09-15 12:40:21 -0500

    fixed issue with DI

--- SystemYouWishYouHad/WishSystem.External/Config/RegisterServices.cs
+++ SystemYouWishYouHad/WishSystem.External/Config/RegisterServices.cs
@@ -6,13 +6,11 @@ public static class RegisterServices
 {
     public static IServiceCollection AddExternalClients(this IServiceCollection services)
     {
-        services.AddHttpClient<IExternalClient>(c =>
+        services.AddHttpClient<IExternalClient, ExternalClient>(c =>
         {
-            c.BaseAddress = new Uri("https://postman-echo.com/");
+            c.BaseAddress = new Uri("https://postman-echo.com");
             c.DefaultRequestHeaders.Add("Accept", "application/json");
         });
-        
-        services.AddTransient<IExternalClient, ExternalClient>();
         
         return services;
     }
