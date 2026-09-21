bd2705c3921b54d14781d1d9945ac1169675b7cd
Jon Graf <jgraf@vermeer.com>
2026-09-21 12:08:54 -0500

    stoppped returning the list to simplify messaging

--- SystemYouWishYouHad/WishSystem.External/ExternalClient.cs
+++ SystemYouWishYouHad/WishSystem.External/ExternalClient.cs
@@ -1,42 +1,32 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json;
-using Amazon.SQS;
-using WishSystem.Shared.Config;
-using WishSystem.External.Model;
+using WishSystem.External.Config;
 
 namespace WishSystem.External;
 
 public interface IExternalClient
 {
-    Task<List<Guid>> Submit(List<Guid> users);
+    Task Submit(List<Guid> users);
 }
 
 public class ExternalClient(HttpClient httpClient) : IExternalClient
 {
-    public async Task<List<Guid>> Submit(List<Guid> users)
+    public async Task Submit(List<Guid> users)
     {
         var response = await httpClient.PostAsJsonAsync("post", users);
 
         response.EnsureSuccessStatusCode();
-        
-        var content =  await response.Content.ReadFromJsonAsync<ExternalResponse>();
-        
-        Console.WriteLine("----- Submitted Successfully -----");
 
-        return content?.Data ?? new List<Guid>();
+        Console.WriteLine("----- Submitted Successfully -----");
     }
 }
 
-public class SqsExternalClient(IAmazonSQS sqs, AppSettings settings) : IExternalClient
+public class SqsExternalClient(SqsQueue queue) : IExternalClient
 {
-    public async Task<List<Guid>> Submit(List<Guid> users)
+    public async Task Submit(List<Guid> users)
     {
-        var queueUrl = (await sqs.CreateQueueAsync(settings.Sqs.QueueName)).QueueUrl;
-
-        await sqs.SendMessageAsync(queueUrl, JsonSerializer.Serialize(users));
+        await queue.Client.SendMessageAsync(queue.Url, JsonSerializer.Serialize(users));
 
         Console.WriteLine("----- Queued Successfully -----");
-        
-        return users;
     }
 }
