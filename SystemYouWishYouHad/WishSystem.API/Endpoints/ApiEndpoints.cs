fc54d004af05e89456156de410cbebda0435b13b
Jon Graf <jgraf@vermeer.com>
2026-09-21 12:24:52 -0500

    actually decided I dont need to discuss return code types

--- SystemYouWishYouHad/WishSystem.API/Endpoints/ApiEndpoints.cs
+++ SystemYouWishYouHad/WishSystem.API/Endpoints/ApiEndpoints.cs
@@ -6,7 +6,7 @@ public static class ApiEndpoints
 {
     public static WebApplication RegisterEndpoints(this WebApplication app)
     {
-        app.MapGet("/users", (IUserService service) => Results.Ok(service.GetUsers()));
+        app.MapGet("/users", (IUserService service) => service.GetUsers());
 
         app.MapPost("/users/submit", (IUserService service, List<Guid> userId) => service.SubmitUsers(userId));
 
