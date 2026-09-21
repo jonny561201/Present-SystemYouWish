872fbe5c65665f051b8bccd2b87208e52e448e3d
Jon Graf <jgraf@vermeer.com>
2026-09-21 13:25:02 -0500

    removed unnecessary properties to streamline project

--- SystemYouWishYouHad/WishSystem.Shared/Config/AppSettings.cs
+++ SystemYouWishYouHad/WishSystem.Shared/Config/AppSettings.cs
@@ -2,8 +2,6 @@ namespace WishSystem.Shared.Config;
 
 public record AppSettings
 {
-    public required string Environment { get; init; }
-    public string[] AllowedOrigins { get; init; } = [];
     public required UserDatabase UserDatabase { get; init; }
     public required BaseEndpoints BaseEndpoints { get; init; }
 }
